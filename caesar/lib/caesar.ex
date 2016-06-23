0606b3c13a6b914ab93449b2db7a032c84adc7b5
JayChetty <jaypaulchetty@gmail.com>
2016-06-23 10:42:08 +0100

    update caesar

--- caesar/lib/caesar.ex
+++ caesar/lib/caesar.ex
@@ -1,2 +1,26 @@
 defmodule Caesar do
+  def main(argv) do
+    argv
+      |> parse_args
+      |> process
+    System.halt(0)
+  end
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [help: :boolean])
+    case parse do
+      {[help: true], _________ , _} -> {:help}
+      {[shift: shift], ["encrypt", msg], _} ->{:encrypt, msg, shift |> String.to_integer}
+      ______ -> {:help}
+    end
+  end
+
+  def process( {:encrypt, msg, shift} )do
+    Caesar.cipher.encrypt(msg, shift)
+      |> IO.puts
+  end
+
+  def process({:help}) do
+    IO.puts
+  end
 end
